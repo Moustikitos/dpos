2c8e1ec665ca200a53fd04a17f2004b4820db57a
Moustikitos <bruno.thoorens@free.fr>
2018-07-27 14:12:33 +0200

    Unifying templates improved layout improved structure

--- wallet/app/app.py
+++ wallet/app/app.py
@@ -1,16 +1,18 @@
-# -*- encoding:utf-8 -*-
+# -*- coding:utf-8 -*-
 import os
+import io
 import sys
 import time
+import json
 import flask
 import random
 import logging
 import optparse
 import datetime
-import webbrowser
 
 import pytz
 import arky
+from six import PY3
 from arky import rest
 from arky import cfg
 
@@ -26,11 +28,12 @@ rest.use(options.network)
 # needed global var
 ROOT = os.path.abspath(os.path.dirname(__file__))
 KEYS = {}
-ACCOUNT = {}
+TX_GENESIS = {}
+CURRENT_TX = {}
 LOGGED = False
 
 # create the application instance 
-app = flask.Flask("DPOS wallet") 
+app = flask.Flask("DPOS wallet [%s]" % options.network) 
 app.config.update(
 	# 600 seconds = 10 minutes lifetime session
 	PERMANENT_SESSION_LIFETIME = 300,
@@ -44,117 +47,148 @@ app.config.update(
 	# update cookies on each request
 	# cookie are outdated after PERMANENT_SESSION_LIFETIME seconds of idle
 	SESSION_REFRESH_EACH_REQUEST = True,
+	# 
 	TEMPLATES_AUTO_RELOAD = True
 )
 
+def loadJson(path):
+	"""Load JSON data from path"""
+	if os.path.exists(path):
+		with io.open(path) as in_:
+			data = json.load(in_)
+	else:
+		data = {}
+	return data
+
+
+def dumpJson(data, path):
+	"""Dump JSON data to path"""
+	try:
+		os.makedirs(os.path.dirname(path))
+	except:
+		pass
+	with io.open(path, "w" if PY3 else "wb") as out:
+		json.dump(data, out, indent=4)
+
+
+def register(tx):
+	"""Write transaction in a registry"""
+	spk = tx["senderPublicKey"]
+	id_ = tx["id"]
+	pathfile = os.path.join(ROOT, ".registry", spk)
+	registry = loadJson(pathfile)
+	registry[id_] = tx
+	dumpJson(registry, pathfile)
+
 
 @app.before_request
-def before_request():
-	"""Reset cookie timeout on each request"""
-	flask.session.modified = True
+def update_session():
+	"""Update wallet data if a wallet is logged in"""
 	if flask.session.get("data", False):
-		flask.session["data"].update(**rest.GET.api.accounts(address=flask.session["data"]["address"]).get("account", {}))
+		address = flask.session["data"]["address"]
+		flask.session["data"].update(**rest.GET.api.accounts(address=address).get("account", {}))
 		flask.session["data"].update(**rest.GET.api.delegates.get(publicKey=KEYS["publicKey"]).get("delegate", {}))
+		flask.session["data"]["voted"] = rest.GET.api.accounts.delegates(address=address).get("delegates", [])
 
-	
+
 @app.route("/", methods=["GET", "POST"])
 def login():
-	global KEYS, ACCOUNT, LOGGED
+	global KEYS, LOGGED
 	flask.session["permanent"] = True
 
-	# if LOGGED true and login called
-	# --> session expired
+	# if LOGGED true and login called --> session expired
 	if LOGGED:
 		flask.flash("Session expired !", category="warning")
+		KEYS.clear()
+		flask.session.clear()
+		LOGGED = False
 	
+	# 
 	if flask.request.method == "POST":
-		# check if address matches with publicKey computed from secret
-		address = flask.request.form["address"]
 		secret = flask.request.form["secret"]
 		KEYS = arky.core.crypto.getKeys(secret)
-		ACCOUNT = rest.GET.api.accounts(address=address).get("account", {})
-		ACCOUNT.update(**rest.GET.api.delegates.get(publicKey=KEYS["publicKey"]).get("delegate", {}))
-		ACCOUNT["voted"] = rest.GET.api.accounts.delegates(address=address).get("delegates", [])
+		address = arky.core.crypto.getAddress(KEYS["publicKey"])
+		# get info from address and public key
+		account = rest.GET.api.accounts(address=address).get("account", {})
+		account.update(**rest.GET.api.delegates.get(publicKey=KEYS["publicKey"]).get("delegate", {}))
+		account["voted"] = rest.GET.api.accounts.delegates(address=address).get("delegates", [])
+		account["address"] = address
 
-		# if address matches with publicKey, set session cookies 
-		if address == arky.core.crypto.getAddress(KEYS["publicKey"]):
-			flask.flash('You are now logged to %s wallet...' % address, category="success")
-			flask.session["data"] = ACCOUNT
-			LOGGED = True
-		# 
-		else:
-			flask.session.pop("data", None)
-			flask.flash('Login failed !', category="error")
-		flask.session["secondPublicKey"] = ACCOUNT.get("secondPublicKey", False)
-	
+		flask.flash('You are now logged to %s wallet...' % address, category="success")
+		flask.session["secondPublicKey"] = account.get("secondPublicKey", False)
+		flask.session["data"] = account
+		LOGGED = True
+
+	# if data is set to session --> login successfull
 	if flask.session.get("data", False):
 		return flask.render_template("account.html")
 	else:
 		KEYS.clear()
-		ACCOUNT.clear()
 		LOGGED = False
 		return flask.render_template("login.html")
 
+
 @app.route("/logout")
 def logout():
-	global KEYS, ACCOUNT, LOGGED
-	flask.session.clear()
-	# .pop("secondPublicKey", None)
-	# flask.session.pop("_flashes", None)
-	# flask.session.pop("explorer", None)
-	# flask.session.pop("begin", None)
-	# flask.session.pop("peer", None)
-	# flask.session.pop("data", None)
+	global KEYS, LOGGED, CURRENT_TX, TX_GENESIS
 	KEYS.clear()
-	ACCOUNT.clear()
+	CURRENT_TX.clear()
+	TX_GENESIS.clear()
 	LOGGED = False
+	flask.session.clear()
 	return flask.render_template("login.html")
+
 
 @app.route("/send", methods=["POST", "GET"])
 def send():
+	global KEYS, TX_GENESIS
+
+	if flask.session.get("secondPublicKey", False):
+		return flask.redirect(flask.url_for("unlock"))
 
 	if flask.request.method == "POST":
-		if "2ndSecret" in flask.request.form:
-			secondSecret = flask.request.form["secondSecret"]
-			keys = arky.core.crypto.getKeys(secondSecret)
-			if keys["publicKey"] == flask.session["secondPublicKey"]:
-				KEYS["secondPublicKey"] = keys["publicKey"]
-				KEYS["secondPrivateKey"] = keys["privateKey"]
-				flask.session["secondPublicKey"] = False
-				flask.flash('Second public key set !', category="success")
-			else:
-				flask.flash('Second public key does not match !', category="error")
-		
-		else:
-			args = dict(
-				recipientId=flask.request.form["recipientId"],
-				amount=float(flask.request.form["amount"])*100000000,
-				vendorField=flask.request.form["vendorField"],
-				publicKey=KEYS.get("publicKey", None),
-				privateKey=KEYS.get("privateKey", None),
-				secondPrivateKey=KEYS.get("secondPrivateKey", None)
-			)
-			try:
-				result = arky.core.sendTransaction(**args)
-			except Exception as e:
-				result = {"messages": ["API error: %s" % e.message]}
-			if len(result.get('transactions', [])):
-				flask.flash('Transaction successfully sent !', category="success")
-			else:
-				flask.flash("<br/>".join(result.get("messages", ["Error occured !"])), category="error")
-		return flask.render_template("send.html")
+		TX_GENESIS = dict(
+			recipientId=flask.request.form["recipientId"],
+			amount=float(flask.request.form["amount"])*100000000,
+			vendorField=flask.request.form["vendorField"],
+			publicKey=KEYS.get("publicKey", None),
+			privateKey=KEYS.get("privateKey", None),
+			secondPrivateKey=KEYS.get("secondPrivateKey", None)
+		)
+		return flask.redirect(flask.url_for("create"))
 	else:
 		if not flask.session.get("data", False):
 			return flask.redirect(flask.url_for("login"))
 		return flask.render_template("send.html")
+
 
 @app.route("/account")
 def account():
 	if not flask.session.get("data", False):
 		return flask.redirect(flask.url_for("login"))
 	else:
-		return flask.render_template("account.html")
+		pathfile = os.path.join(ROOT, ".registry", KEYS["publicKey"])
+		return flask.render_template("account.html", registry=loadJson(pathfile))
 
+
+@app.route("/account/unlock", methods=["GET", "POST"])
+def unlock():
+	if flask.request.method == "POST":
+		secondSecret = flask.request.form["secondSecret"]
+		keys = arky.core.crypto.getKeys(secondSecret)
+		if keys["publicKey"] == flask.session["secondPublicKey"]:
+			KEYS["secondPublicKey"] = keys["publicKey"]
+			KEYS["secondPrivateKey"] = keys["privateKey"]
+			flask.session["secondPublicKey"] = False
+			flask.flash('Second public key set !', category="success")
+			return flask.redirect(flask.url_for("account"))
+		else:
+			flask.flash('Second public key does not match !', category="error")
+			return flask.redirect(flask.url_for("unlock"))
+	else:
+		return flask.render_template("unlock.html")
+
+		
 @app.route("/history")
 def history():
 	if not flask.session.get("data", False):
@@ -165,6 +199,7 @@ def history():
 		flask.session["begin"] = (cfg.begintime - datetime.datetime(1970,1,1, tzinfo=pytz.UTC)).total_seconds()
 		return flask.render_template("history.html")
 
+
 @app.route("/vote")
 def vote():
 	if not flask.session.get("data", False):
@@ -172,20 +207,58 @@ def vote():
 	else:
 		return flask.render_template("vote.html")
 
+
 @app.route("/transactions")
 def txlist():
 	return flask.render_template("txlist.html")
 
-##
+
+@app.route("/tx/init")
+def create():
+	global TX_GENESIS, CURRENT_TX
+	try:
+		CURRENT_TX = arky.core.bakeTransaction(**TX_GENESIS)
+		flask.flash("Check transaction details...", category="warning")
+		return flask.render_template("check.html", tx=CURRENT_TX)
+	except Exception as e:
+		flask.flash("API error: %s" % e.message, category="error")
+		return flask.render_template("send.html")
+
+
+@app.route("/tx/cancel")
+def cancel():
+	global TX_GENESIS, CURRENT_TX
+	TX_GENESIS.clear()
+	CURRENT_TX.clear()
+	flask.flash("Transaction(s) Cancelled", category="warning")
+	return flask.redirect(flask.url_for("account"))
+
+
+@app.route("/tx/confirm")
+def confirm():
+	global CURRENT_TX
+	register(CURRENT_TX)
+	try:
+		result = arky.core.sendPayload(CURRENT_TX)
+	except Exception as e:
+		result = {"messages": ["API error: %s" % e.message]}
+	if len(result.get('transactions', [])):
+		flask.flash('Transaction(s) successfully sent:<br/>%s' % "<br/>".join(result["transaction"]), category="success")
+	else:
+		flask.flash("<br/>".join(result.get("messages", ["Error occured !"])), category="error")
+	return flask.redirect(flask.url_for("account"))
+
+
+###
 @app.context_processor
 def override_url_for():
 	return dict(url_for=dated_url_for)
-	
+
+
 def dated_url_for(endpoint, **values):
 	if endpoint == 'static':
 		filename = values.get('filename', None)
 		if filename:
-			file_path = os.path.join(app.root_path,
-									 endpoint, filename)
+			file_path = os.path.join(app.root_path, endpoint, filename)
 			values['q'] = int(os.stat(file_path).st_mtime)
 	return flask.url_for(endpoint, **values)
