2ce927280ba276e12f9725a3f2af676bc0275a7c
Moustikitos <bruno.thoorens@free.fr>
2019-01-02 23:36:37 +0100

    api optimisations

--- dposlib/ark/v2/api.py
+++ dposlib/ark/v2/api.py
@@ -5,68 +5,42 @@
 
 
 import dposlib
-
-from dposlib import rest
 from dposlib.util.data import filter_dic
-from dposlib.blockchain import Data, Transaction
 
 
-class Wallet(Data):
+class Wallet(dposlib.blockchain.Wallet):
 	
-	link = staticmethod(Transaction.link)
-	unlink = staticmethod(Transaction.unlink)
-
 	def __init__(self, address, **kw):
-		Data.__init__(self, rest.GET.api.wallets, address, **dict({"returnKey":"data"}, **kw))
-
-	def lastTransactions(self, limit=50):
-		received, sent, count = [], [], 0
-		while count < limit:
-			sent.extend(rest.GET.api.transactions(senderId=self.address, orderBy="timestamp:desc", returnKey="data", offset=len(sent)))
-			received.extend(rest.GET.api.transactions(recipientId=self.address, orderBy="timestamp:desc", returnKey="data", offset=len(received)))
-			tmpcount = len(sent)+len(received)
-			count = limit if count == tmpcount else tmpcount
-		return [filter_dic(dic) for dic in sorted(received+sent, key=lambda e:e["timestamp"].get("epoch", None), reverse=True)[:limit]]
-
-	def send(self, amount, recipientId, vendorField=None, **kw):
-		# create a type-1-transaction
-		tx = Transaction(type=0, amount=amount*100000000, recipientId=recipientId, vendorField=vendorField, **kw)
-		# sign if a public and private keys exists
-		try: tx.finalize()
-		# if no key: return orphan tx
-		except: return tx
-		# else broadcast tx
-		else: return rest.POST.api.transactions(transactions=[tx])
+		dposlib.blockchain.Data.__init__(self, dposlib.rest.GET.api.wallets, address, **dict({"returnKey":"data"}, **kw))
 
 
-class Delegate(Data):
+class Delegate(dposlib.blockchain.Delegate):
 
 	def __init__(self, username, **kw):
-		Data.__init__(self, rest.GET.api.delegates, username, **dict({"returnKey":"data"}, **kw))
+		dposlib.blockchain.Data.__init__(self, dposlib.rest.GET.api.delegates, username, **dict({"returnKey":"data"}, **kw))
 
 	def forged(self):
-		result = filter_dic(rest.GET.api.delegates.forging.getForgedByAccount(generatorPublicKey=self.publicKey))
-		result.pop("success", False)
-		return result
+		return self._Data__dict["forged"]
 
 	def voters(self):
-		voters = [a for a in rest.GET.api.delegates(self.username, "voters", returnKey="data") if a["balance"] not in [0, "0"]]
+		voters = [a for a in dposlib.rest.GET.api.delegates(self.username, "voters", returnKey="data") if a["balance"] not in [0, "0"]]
 		return list(sorted([filter_dic(dic) for dic in voters], key=lambda e:e["balance"], reverse=True))
 	
 	def lastBlocks(self, limit=50):
-		return rest.GET.api.delegates(self.username, "blocks", returnKey="data")[:limit]
+		return dposlib.rest.GET.api.delegates(self.username, "blocks", returnKey="data")[:limit]
 
 	def lastBlock(self):
-		return Block(self.blocks["last"]["id"])
+		if self.blocks.get("last", False):
+			return Block(self.blocks["last"]["id"])
 
 
-class Block(Data):
+class Block(dposlib.blockchain.Block):
 
 	def __init__(self, blk_id, **kw):
-		Data.__init__(self, rest.GET.api.blocks, blk_id, **dict({"returnKey":"data"}, **kw))
+		dposlib.blockchain.Data.__init__(self, dposlib.rest.GET.api.blocks, blk_id, **dict({"returnKey":"data"}, **kw))
 
 	def previous(self):
 		return Block(self._Data__dict["previous"])
 
 	def transactions(self):
-		return rest.GET.api.blocks(self.id, "transactions", returnKey="data")
+		return dposlib.rest.GET.api.blocks(self.id, "transactions", returnKey="data")
--- dposlib/blockchain/__init__.py
+++ dposlib/blockchain/__init__.py
@@ -250,6 +250,17 @@ class Data:
 	EVENT = False
 	TRACK = True
 
+	@staticmethod
+	def wallet_islinked(func):
+		def wrapper(*args, **kw):
+			obj = args[0]
+			if getattr(Transaction, "_publicKey", None) == obj.publicKey and \
+			   getattr(Transaction, "_secondPublicKey", None) == obj.secondPublicKey:
+				return func(*args, **kw)
+			else:
+				raise Exception("wallet not linked yet or publicKey mismatch")
+		return wrapper
+
 	@setInterval(30)
 	def heartbeat():
 		for ref in list(Data.REF):
@@ -289,3 +300,73 @@ class Data:
 
 	def track(self):
 		Data.REF.add(weakref.ref(self))
+
+
+##### create api V1 objects ######
+
+class Wallet(Data):
+	
+	link = staticmethod(lambda s,ss=None: [Transaction.unlink(), Transaction.link(s,ss)][0])
+	unlink = staticmethod(Transaction.unlink)
+
+	def __init__(self, address, **kw):
+		dposlib.blockchain.Wallet.__init__(self, dposlib.rest.GET.api.accounts, **dict({"address":address, "returnKey":"account"}, **kw))
+
+	def lastTransactions(self, limit=50):
+		received, sent, count = [], [], 0
+		while count < limit:
+			sent.extend(dposlib.rest.GET.api.transactions(senderId=self.address, orderBy="timestamp:desc", returnKey="transactions", offset=len(sent)))
+			received.extend(dposlib.rest.GET.api.transactions(recipientId=self.address, orderBy="timestamp:desc", returnKey="transactions", offset=len(received)))
+			tmpcount = len(sent)+len(received)
+			count = limit if count == tmpcount else tmpcount
+		return [filter_dic(dic) for dic in sorted(received+sent, key=lambda e:e.get("timestamp", None), reverse=True)[:limit]]
+
+	@Data.wallet_islinked
+	def send(self, amount, address, vendorField=None, fee_included=False):
+		tx = dposlib.core.transfer(amount, address, vendorField)
+		tx.finalize(fee_included=fee_included)
+		return dposlib.rest.POST.api.transactions(transactions=[tx])
+
+	@Data.wallet_islinked
+	def upVote(self, *usernames):
+		tx = dposlib.core.upVote(*usernames)
+		tx.finalize()
+		return dposlib.rest.POST.api.transactions(transactions=[tx])
+
+	@Data.wallet_islinked
+	def downVote(self, *usernames):
+		tx = dposlib.core.downVote(*usernames)
+		tx.finalize()
+		return dposlib.rest.POST.api.transactions(transactions=[tx])
+
+
+class Delegate(Data):
+	
+	def __init__(self, username, **kw):
+		Data.__init__(self, dposlib.rest.GET.api.delegates.get, **dict({"username":username, "returnKey":"delegate"}, **kw))
+
+	def forged(self):
+		result = filter_dic(dposlib.rest.GET.api.delegates.forging.getForgedByAccount(generatorPublicKey=self.publicKey))
+		result.pop("success", False)
+		return result
+
+	def voters(self):
+		voters = [a for a in dposlib.rest.GET.api.delegates.voters(publicKey=self.publicKey, returnKey="accounts") if a["balance"] not in [0, "0"]]
+		return list(sorted([filter_dic(dic) for dic in voters], key=lambda e:e["balance"], reverse=True))
+	
+	def lastBlock(self):
+		blocks = [blk for blk in dposlib.rest.GET.api.blocks(returnKey="blocks") if blk["generatorId"] == self.address]
+		if len(blocks):
+			return Block(blocks[0]["id"])
+
+
+class Block(Data):
+
+	def __init__(self, blk_id, **kw):
+		Data.__init__(self, dposlib.rest.GET.api.blocks.get, **dict({"id":blk_id, "returnKey":"block"}, **kw))
+
+	def previous(self):
+		return Block(self.previousBlock)
+
+	def transactions(self):
+		return dposlib.rest.GET.api.transactions(blockId=self.id, returnKey="transactions")
