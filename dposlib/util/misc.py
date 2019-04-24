5352f9f20cf3776a19474a9bb070c60478066a1c
Moustikitos <bruno.thoorens@free.fr>
2019-04-24 13:14:22 +0200

    minor compatibility changes ripa network change

--- dposlib/util/misc.py
+++ dposlib/util/misc.py
@@ -36,20 +36,20 @@ def deltas():
 	delegates = loadPages(rest.GET.api.delegates)
 	blocks = [d["blocks"] for d in delegates]
 	produced = sum(b["produced"] for b in blocks)
-	missed = sum(b["missed"] for b in blocks)
+	# missed = sum(b["missed"] for b in blocks)
 
 	last_block_timestamp = slots.getRealTime(delegates[0]["blocks"]["last"]["timestamp"]["epoch"])
 	total_elapsed_time = (last_block_timestamp - rest.cfg.begintime).total_seconds()
-	total_blocks = produced + missed
+	# total_blocks = produced + missed
 
 	theorical_height = int((datetime.datetime.now(pytz.UTC) - rest.cfg.begintime).total_seconds() / rest.cfg.blocktime)
 
 	return OrderedDict({
 		"real blocktime": total_elapsed_time / produced,
-		"failureless blocktime": total_elapsed_time / total_blocks,
-		"block missed": missed,
+		# "failureless blocktime": total_elapsed_time / total_blocks,
+		# "block missed": missed,
 		"block produced (real height)": produced,
 		"theorical height": theorical_height,
 		"height shift": produced - theorical_height,
-		"failureless height shift": produced + missed - theorical_height
+		# "failureless height shift": produced + missed - theorical_height
 	})
