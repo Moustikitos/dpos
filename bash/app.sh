037fb55b3ea752a16ba141ba3501e53dbf96f9a2
unknown <moustikitos@gmail.com>
2020-01-03 22:43:32 +0100

    msapp launcher fix

--- bash/app.sh
+++ bash/app.sh
@@ -1,3 +1,3 @@
 . ~/.local/share/ms-server/venv/bin/activate
 export PYTHONPATH=${PYTHONPATH}:${HOME}/dpos
-gunicorn --bind=$1:$2 --workers=5 'mssrv.app.srv:create_app()'
+gunicorn --bind=$1:$2 --workers=5 'mssrv.app.srv:create_app(mssrv="$3")'
--- mssrv/app/srv.py
+++ mssrv/app/srv.py
@@ -6,26 +6,26 @@ Usage:
    srv [-h <host> -p <port> -m <peer> -d]
 
 Options:
--d --debug          : debug mode                   [default: False]
--p --port=<port>    : the port you want to use     [default: 5001]
+-d --debug          : debug mode  [default: False]
+-p --port=<port>    : the port you want to use  [default: 5001]
 -h --host=<host>    : the host ip you want to use  [default: 127.0.0.1]
--m --ms-peer=<peer> : multisignature server peer   [default: http://127.0.0.1:5000]
+-m --ms-peer=<peer> : multisignature server peer  [default: http://127.0.0.1:5000]
 """
 
 import sys
 import docopt
 
 from mssrv.app import app, _ark_srv_synch, _link_peer
-args = docopt.docopt(__doc__, argv=sys.argv[1:])
 
 
-def create_app():
-    app.config.update(DEBUG=args["--debug"])
-    _link_peer(args["--ms-peer"])
+def create_app(mssrv):
+    _link_peer(mssrv)
     _ark_srv_synch()
     return app
 
 
 if __name__ == "__main__":
+    args = docopt.docopt(__doc__, argv=sys.argv[1:])
     create_app()
+    app.config.update(DEBUG=args["--debug"])
     app.run(host=args["--host"], port=int(args["--port"]))
