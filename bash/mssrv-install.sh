7455536890da3ff82fc43ba6dba3985b1699616c
unknown <moustikitos@gmail.com>
2020-03-27 11:19:09 +0100

    install tidy

--- bash/mssrv-install.sh
+++ bash/mssrv-install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+VENVDIR="$HOME/.local/share/ms-server/venv"
+GITREPO="https://github.com/Moustikitos/dpos.git"
+
 clear
 
 if [ $# = 0 ]; then
@@ -11,6 +15,7 @@ echo "github branch to use : $B"
 echo
 echo installing system dependencies
 echo ==============================
+sudo apt-get -qq install libudev-dev libusb-1.0.0-dev
 sudo apt-get -qq install python python-dev python-setuptools python-pip
 sudo apt-get -qq install python3 python3-dev python3-setuptools python3-pip
 sudo apt-get install pypy
@@ -19,12 +24,14 @@ sudo apt-get install virtualenv
 echo
 echo downloading dpos package
 echo ========================
+
 cd ~
-if (git clone -q --branch $B https://github.com/Moustikitos/dpos.git) then 
-    echo "cloning dpos..."
+if (git clone --branch $B $GITREPO) then
+    echo "package cloned !"
 else
-    echo "dpos already cloned !"
+    echo "package already cloned !"
 fi
+
 cd ~/dpos
 git reset --hard
 git fetch --all
@@ -40,32 +47,40 @@ echo
 echo creating virtual environement
 echo =============================
 
-echo -e "Select the environment:\n  1) python3\n  2) pypy"
-read -p "(default:python)> " n
-case $n in
-1) TARGET="$(which python3)";;
-2) TARGET="$(which pypy)";;
-*) TARGET="$(which python)";;
-esac
-
-if [ ! -d "$HOME/.local/share/ms-server/venv" ]; then
-    mkdir ~/.local/share/ms-server/venv -p
-    virtualenv -p ${TARGET} ~/.local/share/ms-server/venv -q
-    echo "done"
-else
-    echo "virtual environement already there !"
+if [ -d $VENVDIR ]; then
+    read -p "remove previous virtual environement ? [y/N]> " r
+    case $r in
+    y) rm -rf $VENVDIR;;
+    Y) rm -rf $VENVDIR;;
+    *) echo -e "previous virtual environement keeped";;
+    esac
 fi
+
+if [ ! -d $VENVDIR ]; then
+    echo -e "select environment:\n  1) python3\n  2) pypy"
+    read -p "[default:python]> " n
+    case $n in
+    1) TARGET="$(which python3)";;
+    2) TARGET="$(which pypy)";;
+    *) TARGET="$(which python)";;
+    esac
+    mkdir $VENVDIR -p
+    virtualenv -p $TARGET $VENVDIR -q
+fi
+
+echo "done"
 
 echo
 echo installing python dependencies
 echo ==============================
-. ~/.local/share/ms-server/venv/bin/activate
+. $VENVDIR/bin/activate
 export PYTHONPATH=${HOME}/dpos
 pip install -r ~/dpos/requirements.txt
 pip install docopt gunicorn flask
 echo "done"
 
 chmod +x bash/activate
+
 cp bash/ms ~
 cd ~
 chmod +x ms
