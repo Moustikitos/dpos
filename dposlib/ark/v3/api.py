20ab0891d411792995618c868ae9ab37484a7040
Moustikitos <moustikitos@gmail.com>
2021-09-05 15:23:44 +0200

    multivote and magistrate added to api wallet

--- dposlib/ark/v3/api.py
+++ dposlib/ark/v3/api.py
@@ -11,10 +11,25 @@ class Wallet(Wallet):
 
     @dposlib.blockchain.isLinked
     def upVote(self, *usernames):
-        "See [`dposlib.ark.v2.upVote`](blockchain.md#upvote)."
+        "See [`dposlib.ark.v3.multiVote`](v3.md#multivote)."
         tx = dposlib.core.upVote(*usernames)
         if self.attributes.vote is not None:
             tx["asset"]["votes"].insert(0, "-" + self.attributes.vote)
+        return dposlib.core.broadcastTransactions(self._finalizeTx(tx))
+
+    def createEntity(self, name, type="business", subtype=0, ipfsData=None):
+        "See [`dposlib.ark.v3.entityRegister`](v3.md#entityregister)."
+        tx = dposlib.core.entityRegister(name, type, subtype, ipfsData)
+        return dposlib.core.broadcastTransactions(self._finalizeTx(tx))
+
+    def updateEntity(self, registrationId, ipfsData, name=None):
+        "See [`dposlib.ark.v3.entityUpdate`](v3.md#entityupdate)."
+        tx = dposlib.core.entityUpdate(registrationId, ipfsData, name)
+        return dposlib.core.broadcastTransactions(self._finalizeTx(tx))
+
+    def resignEntity(self, registrationId):
+        "See [`dposlib.ark.v3.entityResign`](v3.md#entityresign)."
+        tx = dposlib.core.entityResign(registrationId)
         return dposlib.core.broadcastTransactions(self._finalizeTx(tx))
 
 
