1927787fa835125ea9a90187c79aec263e3d3e0b
Moustikitos <moustikitos@gmail.com>
2022-05-28 08:04:22 +0200

    removed inactive networks

--- dposlib/net.py
+++ dposlib/net.py
@@ -37,85 +37,6 @@ dark = {
     "seeds": ["https://dwallets.ark.io"]
 }
 
-# iop = {
-#     "familly": "ark",
-#     "aip20": 0,
-#     "txversion": 2,
-
-#     "seeds": [
-#         "http://35.195.150.223:4703",
-#         "http://34.76.165.50:4703",
-#         "http://104.155.17.122:4703",
-#         "http://35.228.202.124:4703",
-#         "http://35.228.73.165:4703",
-#         "http://35.198.174.42:4703",
-#         "http://35.246.135.62:4703",
-#         "http://34.90.0.113:4703",
-#         "http://34.90.43.152:4703",
-#         "http://34.66.138.96:4703",
-#         "http://104.154.142.55:4703",
-#         "http://34.74.102.192:4703",
-#         "http://35.231.24.181:4703",
-#         "http://35.233.159.123:4703",
-#         "http://35.230.119.77:4703",
-#         "http://35.235.109.207:4703",
-#         "http://34.94.151.12:4703",
-#         "http://35.203.123.223:4703",
-#         "http://35.240.251.207:4703",
-#         "http://35.190.233.247:4703",
-#         "http://34.93.248.166:4703"
-#     ]
-# }
-
-# diop = {
-#     "familly": "ark",
-#     "aip20": 0,
-#     "txversion": 2,
-
-#     "seeds": [
-#         "http://35.204.124.143:4703",
-#         "http://34.68.118.161:4703",
-#         "http://35.228.196.114:4703",
-#         "http://34.87.3.205:4703",
-#         "http://35.185.32.241:4703",
-#         "http://35.240.62.119:4703"
-#     ]
-# }
-
-# tqredit = {
-#     "familly": "ark",
-#     "aip20": 0,
-#     "txversion": 2,
-
-#     "seeds": [
-#         "http://35.163.173.247:4103",
-#         "http://116.203.33.4:4103",
-#         "http://116.203.56.119:4103",
-#         "http://116.203.58.165:4103",
-#         "http://159.69.89.111:4103",
-#         "http://159.69.89.41:4103"
-#     ]
-# }
-
-# qredit = {
-#     "familly": "ark",
-#     "aip20": 0,
-#     "txversion": 2,
-
-#     "seeds": ["https://qredit.cloud"]
-# }
-
-# aslp = {
-#     "familly": "aslp",
-#     "aip20": 0,
-#     "txversion": 2,
-
-#     "master_address": "ARKQXzHvEWXgfCgAcJWJQKUMus5uE6Yckr",
-#     "aslp_api": ["https://aslp.qredit.dev"],
-
-#     "seeds": ["https://api.ark.io"]
-# }
-
 nos = {
     "familly": "ark",
     "aip20": 0,
@@ -135,7 +56,7 @@ uns = {
 tsxp = {
     "familly": "ark",
     "aip20": 0,
-    "txversion": 2,
+    "txversion": 3,
 
     "seeds": ["https://sxp.testnet.sh"]
 }
@@ -144,10 +65,6 @@ sxp = {
     "familly": "ark",
     "aip20": 0,
     "txversion": 2,
-    "pubKeyHash": 63,
-    "nethash":
-        "16db20c30c52d53638ca537ad0ed113408da3ae686e2c4bfa7e315d4347196dc",
-    "wif": 252,
-    "slip44": 3333,
-    "seeds": ["http://45.32.148.40:6003"]
+
+    "seeds": ["https://sxp.mainnet.sh"]
 }
