57109862a9d5c43c5169f89621e333159c9e9a7c
Moustikitos <moustikitos@gmail.com>
2022-06-06 16:41:14 +0200

    pkg: update

--- freeze.py
+++ freeze.py
@@ -1,4 +1,7 @@
+
+import os
 import sys
+import Cryptodome
 from cx_Freeze import setup, Executable
 
 sys.argv.append("build_exe")
@@ -8,16 +11,26 @@ setup(
     name="Ark lite wallet",
     version="1",
     description="Easy way to interact with ark blockchain and its forks",
-    executables=[Executable("dposlib/cmd/send.py")],
+    executables=[Executable("dposlib/cmd/send.py", base="console")],
     options={
         "build_exe": {
             "zip_include_packages": [
                 "encodings", "usrv", "base58", "collections",
                 "concurrent", "ctypes", "email", "future", "html", "http",
-                "json", "logging", "urllib", "xml", "importlib"
+                "json", "logging", "urllib", "xml", "importlib", "certifi",
+                "ecpy", "google", "charset_normalizer", "idna", "requests",
+                "websocket", "u2flib_host",
+                "ledgerblue"
             ],
             "excludes": ["test", "unittest", "pydoc_data"],
-            "packages": ["dposlib.ark.cold"]
+            "packages": [
+                "dposlib.ark.cold", "ledgerblue", "urllib3"
+            ]
         }
     }
 )
+
+os.system("upx -9 build/exe.win-amd64-3.10/*.dll")
+os.system("upx -9 build/exe.win-amd64-3.10/lib/*.dll")
+os.system("upx -9 build/exe.win-amd64-3.10/lib/*.pyd")
+os.system("upx -9 build/exe.win-amd64-3.10/lib/cSecp256k1/*.dll")
